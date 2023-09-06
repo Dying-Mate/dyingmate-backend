32f55bfb61ba113223f08ab5e070d7bbb5464747
0lYUMA <102151689+0lYUMA@users.noreply.github.com>
2023-09-06 13:59:07 +0900

    feat: 프로젝트 생성 및 환경설정

---
+++ src/main/java/com/example/dyingmatebackend/DyingmateBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.dyingmatebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DyingmateBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DyingmateBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/dyingmatebackend/DyingmateBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dyingmatebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DyingmateBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
