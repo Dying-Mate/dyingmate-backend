37b77540561d0656beb8c9f5f3cb882bbe6ca90e
0lYUMA <102151689+0lYUMA@users.noreply.github.com>
2023-10-16 14:12:21 +0900

    feat: CorsMapping URL 수정

--- src/main/java/com/example/dyingmatebackend/config/WebConfig.java
+++ src/main/java/com/example/dyingmatebackend/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer { // CORS 설정
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://dying-mate.netlify.app")
+                .allowedOrigins("https://dyingmate.com")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
